70e7310368e14376317e8a7e5c29c905786dbabe
Archer141 <ezio.141.96@gmail.com>
2018-11-09 15:04:47 +0100

    Menu version 0.7

--- Rogal_na_KaCu/Program.cs
+++ Rogal_na_KaCu/Program.cs
@@ -8,17 +8,123 @@ namespace Rogal_na_KaCu
 {
     class Program
     {
+
+        private static int index = 0;
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             DisplayConsole display = new DisplayConsole();
-            //display.DrawFrame();
-            GameHandler gameMaster = new GameHandler(display);
-            Map firstMap=gameMaster.LoadMap("1.txt");
-            //display.DisplayMap(firstMap,0,0);
-            gameMaster.PlayInMap();
-            Console.ReadKey();
+            List<string> menuItems = new List<string>()
+            {
+                "Rozpocznij grę",
+                "Fabuła",
+                "Twórcy",
+                "Wyjście"
+            };
+
+            Console.CursorVisible = false;
+            while (true)
+            {
+                string line;
+                System.IO.StreamReader frameFile = new System.IO.StreamReader("display/dragon.txt");
+                while ((line = frameFile.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+                frameFile.Close();
+
+                string selectedMenu = DrowMainMenu(menuItems);
+                if (selectedMenu == "Rozpocznij grę")
+                {
+                    display.DrawFrame();
+                    GameHandler gameMaster = new GameHandler(display);
+                    Map firstMap = gameMaster.LoadMap("1.txt");
+                    display.DisplayMap(firstMap, 0, 0);
+                    gameMaster.PlayInMap();
+                    Console.ReadKey();
+                }
+                else if (selectedMenu == "Fabuła")
+                {
+                    display.DrowStory();
+                }
+                else if (selectedMenu == "Twórcy")
+                {
+                    display.DrowCredits();
+                }
+                else if (selectedMenu == "Wyjście")
+                {
+                    Environment.Exit(0);
+                }
+                Console.Clear();
+            };
         }
+
+
+
+        public static string DrowMainMenu(List<string> item)
+        {
+            Console.CursorVisible = false;
+            for (int i = 0; i < item.Count; i++)
+            {
+                if (i == index)
+                {
+                    Console.SetCursorPosition(36, 3 + i);
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(item[i]);
+                }
+                else
+                {
+                    Console.SetCursorPosition(36, 3 + i);
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine(item[i]);
+
+                }
+                Console.ResetColor();
+            }
+
+            ConsoleKeyInfo consoleKey = Console.ReadKey();
+
+            if (consoleKey.Key == ConsoleKey.DownArrow)
+            {
+                if (index == item.Count - 1)
+                {
+                    index = 0;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            else if (consoleKey.Key == ConsoleKey.UpArrow)
+            {
+                if (index == 0)
+                {
+                    index = item.Count - 1;
+                }
+                else
+                {
+                    index--;
+                }
+            }
+            else if (consoleKey.Key == ConsoleKey.Enter)
+            {
+                return item[index];
+            }
+            else
+            {
+                return "";
+            }
+            Console.Clear();
+            return "";
+
+     
+
+        }
+
+
     }
 }
